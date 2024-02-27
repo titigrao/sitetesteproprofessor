146fbb0e42539a3033c536d6dc22d1dbcbd10dfa
Titigas <127964804+titigrao@users.noreply.github.com>
2024-02-27 08:29:33 -0300

    asdasdasdadasd

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 function mudarCorDoTexto(){
-    alert("claudio é legal?")
-}
-
-function mudarFundoDoTexto(){
-    alert("claudio é legal!")
+    document.querySelector("#p1").style.color= "green"
 }
 
 
 
-document.querySelector("#p1")
 
-document.querySelector("#p2")
 
 
 
