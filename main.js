74d04d139ae6bbda5c8645759304732a916412f5
Titigas <127964804+titigrao@users.noreply.github.com>
2024-02-27 08:51:26 -0300

    asd

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function mudarCorDoTexto(){
 
-    document.querySelector("#textolegal").style.color= "red"
+    document.querySelector("#textolegal").style.color= "purple"
 }
 
 function mudarFundoDoTexto(){
